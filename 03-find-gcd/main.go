a58a8b6e52f0fc47899b4d86a56da4da484899fc
chandanghosh <ckghosh1983@gmail.com>
2021-11-16 12:12:38 +0530

    Problem statement with helping text added to the problem

--- 03-find-gcd/main.go
+++ 03-find-gcd/main.go
@@ -1,3 +1,12 @@
+// Finding Euclid's greatest common denominator.
+// Let's say you have two numbers 20 and 8, now to find the GCD of this two, we need to
+// find the greatest common number that can devide both of this number so that
+// they don't have a remainder
+// Mathematically, we solve this problem by deviding the largest number by the smaller
+// number and if there is a remainder of the division then the divisor become dividend and the remainder
+// becomes divisor and will divide again. This process continues until we find the remainder to be zero.
+// When remainder becomes zero, the divisor becomes the GCD of the two numbers.
+
 package main
 
 func main() {
