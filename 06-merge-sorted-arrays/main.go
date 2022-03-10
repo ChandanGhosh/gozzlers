077330f62e4c7347014fd111cb3e3c098b6aa8e6
chandanghosh <ckghosh1983@gmail.com>
2022-03-10 05:19:03 +0000

    Revert the doc and added to merge-sort puzzle in 09-merge-sort folder

--- 06-merge-sorted-arrays/main.go
+++ 06-merge-sorted-arrays/main.go
@@ -1,11 +1,4 @@
 // Merge two sorted arrays like [1, 2, 3, 5] and [4, 6, 8, 9]
-// Here, in Merge sort we use the divide and conquer method. Usually the complexity is O(nlogn) or
-// linear logarithmic time. So given you have some
-// array, could be unsorted, then in Merge sort we divide the array until each array contains
-// single element which then we can consider as sorted as the arrays have only one element each
-// then taking two arrays as a and b and merge back in to aother array and continue until all the arrays are taken care.
-// In this process there could be some arrays with more elements than the others while merging back, in that case those array
-// elements should be appended as they are already sorted.
 
 package main
 
@@ -46,6 +39,7 @@ func mergeSortedArrays(a, b []int) []int {
 	// Above this point, we had only compared two arrays and sort based on that
 	// Now what if left array (a) also have some elements left after the other one, the right array
 	// is enumerated or vice versa, we should take care of that scenerios
+
 	// left array elements left
 	if i < len(a) {
 		mergedArr = append(mergedArr, a[i:]...)
